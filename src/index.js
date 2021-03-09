a5343267433e0d43813a8ccf931cd016007b4d74
LugalKiEn <ulasik1996@gmail.com>
2021-03-10 00:54:03 +0300

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    let absN = Math.abs(n);
+    return Number(absN.toString().split("").reverse().join(""))
+//   return Number(n.toString().split("").map(function(item, index) { if (item = "-") { n.splice(index, 1)}}).reverse().join(""));
 }
